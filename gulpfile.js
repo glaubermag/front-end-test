3b1a440c3c474b1c205ae1ed0fa46dcbff7c6c12
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-07-12 15:43:37 -0300

    gulp scripts updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
-const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
-const concat = require('gulp-concat');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
+// Gulp task to minify CSS files
+gulp.task('styles', function() {
+    return gulp.src('./css/estilo.css')
+        // Auto-prefix css styles for cross browser compatibility
+        .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
+        // Minify the file
+        .pipe(csso())
+        // Output
+        .pipe(gulp.dest('./dist/css/'))
+});
 
-function stylesMethod() {
+// Gulp task to minify HTML files
+gulp.task('pages', function() {
+    return gulp.src(['./*.html'])
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
 
-    return gulp.src("./css/*.css")
-        .pipe(concat("all.css"))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("./dist"));
 
-}
-
-exports.styles = stylesMethod;
+// Gulp task to minify img files
+gulp.task('minify', function() {
+    return gulp
+        .src("./img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img"));
+});
